705f2b79dc9c5ad4e3e46b8bbd7504e39dfae34e
explorerezral <87631579+explorerezral@users.noreply.github.com>
2023-04-24 17:22:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask,jsonify,request
+from flask import Flask,request
 import os
 import json
+import requests
 
 app = Flask(__name__)
 
@@ -8,15 +9,36 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/json', methods=['get','POST'])
+@app.route('/chat', methods=['GET','POST'])
 def form():
+    bot = chat()
     if request.method == 'GET':
         return 'This is a GET request to /json'
     if request.method == 'POST':
         data = json.loads(request.data) # 将json字符串转为dict
-        name = data['name']
-        age = data['age']
-        return "user_name = %s, user_age = %s" % (name,age)
+        key = data['key']
+        del data['key']
+        
+        response = chat.create_chatgpt_request(key,data)
+        
+        return response
+    
+class chat ():
+    def create_chatgpt_request(OPENAI_API_KEY,data):
+        url = "https://api.openai.com/v1/chat/completions"
+        model = "gpt-3.5-turbo"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + OPENAI_API_KEY
+        }
+
+        # character = "Humor"
+        #messages=[{"role":"system", "content": "You are a helpful assistant and answer every question by one sentence."},
+     
+        print(data)
+  
+        response_json = requests.post(url, headers=headers, json=data).json()
+        return response_json 
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
