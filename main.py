242c77452a9d114ea797aaf81ba70060db0f3aaf
explorerezral <87631579+explorerezral@users.noreply.github.com>
2023-04-25 16:06:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def audio_process():
     if request.method == 'POST':
         print(f"Request Method: {request.method}")
         print(f"Request URL: {request.url}")
-        
+        request_data = request.get_data().decode('utf-8')
         print("Request Data:") 
         print(request.data)
         # 打印请求头信息
